68f2f59dc6aebb8283213b843204d9e60f2916e2
Luca Kler Lago <66525361+lucalago@users.noreply.github.com>
2021-04-13 13:06:28 +0200

    Update

--- script.js
+++ script.js
@@ -39,19 +39,19 @@ function staticLoadPlaces() {
 var models = [
     {
         url: 'assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
+        scale: '0.8 0.8 0.8',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
     },
     {
         url: 'assets/articuno/scene.gltf',
-        scale: '0.2 0.2 0.2',
+        scale: '0.4 0.4 0.4',
         rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
     {
         url: 'assets/dragonite/scene.gltf',
-        scale: '0.08 0.08 0.08',
+        scale: '0.3 0.3 0.3',
         rotation: '0 180 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
